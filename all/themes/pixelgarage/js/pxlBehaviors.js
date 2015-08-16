fa3d0be412153496b3057f3f34bf9a416a0e67a6
Ralph Moser <ralph@ramosoft.ch>
2015-08-17 00:20:04 +0200

    - filters implemented

--- all/themes/pixelgarage/js/pxlBehaviors.js
+++ all/themes/pixelgarage/js/pxlBehaviors.js
@@ -27,12 +27,82 @@
             var $cards = $('.view-europa .card');
 
             $cards.once('click', function() {
-                $cards.on('click', function() {
+                $(this).on('click', function() {
                     $(this).toggleClass('flipped');
                 });
             });
         }
-    }
+    };
+
+    Drupal.behaviors.filterMenus = {
+        attach: function() {
+            var $pageHeader = $('#page-header'),
+                $secondaryMenu = $('header#navbar .menu.secondary'),
+                $menus = $secondaryMenu.find('.menu-filter');
+
+            $menus.once('click', function() {
+                $(this).on('click', function() {
+                    var $menu = $(this),
+                        $menuIsActive = $menu.hasClass('active'),
+                        $parliamentarianFilter = $('#edit-field-parliamentarian-value-wrapper'),
+                        $cantonFilters = $('#edit-field-canton-tid-wrapper'),
+                        $partyFilters = $('#edit-field-party-tid-wrapper');
+
+                    // reset active menu
+                    $menus.removeClass('active');
+
+                    // menu specific
+                    if ($menu.hasClass('filter-candidates')) {
+
+                    } else if ($menu.hasClass('filter-swiss-canton')) {
+                        $partyFilters.hide();
+                        $cantonFilters.show();
+                        if (!$menuIsActive) {
+                            $menu.addClass('active')
+                        } else {
+                            $menu.removeClass('active')
+                        }
+
+                    } else if ($menu.hasClass('filter-party')) {
+                        $cantonFilters.hide();
+                        $partyFilters.show();
+                        if (!$menuIsActive) {
+                            $menu.addClass('active')
+                        } else {
+                            $menu.removeClass('active')
+                        }
+
+                    }
+
+                    // show / hide filter section
+                    if ($menus.hasClass('active')) {
+                        $pageHeader.slideDown(300);
+                    } else {
+                        $pageHeader.slideUp(300);
+                    }
+                });
+            });
+        }
+    };
+
+    Drupal.behaviors.pxlCheckboxClick = {
+        attach: function () {
+            var $pxlCheckboxes = $('.pxl-checkbox');
+
+            $pxlCheckboxes.once('click', function() {
+                $(this).on('click', function() {
+                    var $checkbox = $(this),
+                        $input = $checkbox.find('input');
+
+                    if ($input.prop('checked')) {
+                        $checkbox.addClass('selected');
+                    } else {
+                        $checkbox.removeClass('selected');
+                    }
+                });
+            });
+        }
+    };
 
     /**
      * Allows full size clickable items.
